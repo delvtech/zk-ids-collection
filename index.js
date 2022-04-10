5c062a77f88cb49964ccefc6d984870f7ee91b21
Ryan Goree <goree.ryan@gmail.com>
2022-04-10 00:17:35 -0500

    Add auto script

--- index.js
+++ index.js
@@ -9,49 +9,56 @@ const { writeJSONFile, dedupeByProperty, writeCSVFile } = require('./util')
 // issues: 384, 724
 const githubIssueId = 724;
 
+const githubCommand = async () => {
+  const whitelist = Object.keys(githubWL)
+  const idSubmissions = await github.getIdSubmissions(githubIssueId)
+  const [unique, dupes] = dedupeByProperty(idSubmissions, 'userId')
+  const invalidSubmissions = []
+  const ineligibleUsers = []
+  const eligible = unique
+    .filter((submission) => {
+      if (submission.invalidSubmission) {
+        invalidSubmissions.push(submission)
+        return false
+      }
+      return true
+    })
+    .filter((submission) => {
+      const isEligible = whitelist.includes(submission.user)
+      if (!isEligible) {
+        ineligibleUsers.push(submission)
+      }
+      return isEligible
+    })
+  const idSubmissionUsers = unique.map((user) => user.user)
+  const missingEligible = whitelist
+    .filter((user) => !idSubmissionUsers.includes(user))
+    .map((user) => ({ user }))
+  writeCSVFile(`extra/csv/github_invalid_issue-${githubIssueId}.csv`, invalidSubmissions)
+  writeCSVFile(`extra/csv/github_eligible_missing_issue-${githubIssueId}.csv`, missingEligible)
+  writeCSVFile(`extra/csv/github_eligible_issue-${githubIssueId}.csv`, eligible)
+  writeJSONFile(`extra/json/github_dupes_issue-${githubIssueId}.json`, dupes)
+  writeJSONFile(`extra/json/github_invalid_issue-${githubIssueId}.json`, invalidSubmissions)
+  writeJSONFile(
+    `extra/json/ineligible/github_ineligible_issue-${githubIssueId}_${Date.now()}.json`,
+    ineligibleUsers
+  )
+  writeJSONFile(`extra/json/github_eligible_missing_issue-${githubIssueId}.json`, missingEligible)
+  writeJSONFile(`results/json/github_issue-${githubIssueId}.json`, unique)
+  writeCSVFile(`results/csv/github_issue-${githubIssueId}.csv`, unique)
+  github.clearIneligibleSubmissions(ineligibleUsers)
+  console.log(
+    `Collected ${idSubmissions.length} submissions from GitHub, filtered down to ${unique.length} unique users, and found ${eligible.length} eligible. Unique submissions in the results directory.`
+  )
+}
+
 const commands = {
-  github: async () => {
-    const whitelist = Object.keys(githubWL)
-    const idSubmissions = await github.getIdSubmissions(githubIssueId)
-    const [unique, dupes] = dedupeByProperty(idSubmissions, 'userId')
-    const invalidSubmissions = []
-    const ineligibleUsers = []
-    const eligible = unique
-      .filter((submission) => {
-        if (submission.invalidSubmission) {
-          invalidSubmissions.push(submission)
-          return false
-        }
-        return true
-      })
-      .filter((submission) => {
-        const isEligible = whitelist.includes(submission.user)
-        if (!isEligible) {
-          ineligibleUsers.push(submission)
-        }
-        return isEligible
-      })
-    const idSubmissionUsers = unique.map((user) => user.user)
-    const missingEligible = whitelist
-      .filter((user) => !idSubmissionUsers.includes(user))
-      .map((user) => ({ user }))
-    writeCSVFile(`extra/csv/github_invalid_issue-${githubIssueId}.csv`, invalidSubmissions)
-    writeCSVFile(`extra/csv/github_eligible_missing_issue-${githubIssueId}.csv`, missingEligible)
-    writeCSVFile(`extra/csv/github_eligible_issue-${githubIssueId}.csv`, eligible)
-    writeJSONFile(`extra/json/github_dupes_issue-${githubIssueId}.json`, dupes)
-    writeJSONFile(`extra/json/github_invalid_issue-${githubIssueId}.json`, invalidSubmissions)
-    writeJSONFile(
-      `extra/json/ineligible/github_ineligible_issue-${githubIssueId}_${Date.now()}.json`,
-      ineligibleUsers
-    )
-    writeJSONFile(`extra/json/github_eligible_missing_issue-${githubIssueId}.json`, missingEligible)
-    writeJSONFile(`results/json/github_issue-${githubIssueId}.json`, unique)
-    writeCSVFile(`results/csv/github_issue-${githubIssueId}.csv`, unique)
-    github.clearIneligibleSubmissions(ineligibleUsers)
-    console.log(
-      `Collected ${idSubmissions.length} submissions from GitHub, filtered down to ${unique.length} unique users, and found ${eligible.length} eligible. Unique submissions in the results directory.`
-    )
+  autoGithub: async () => {
+    setInterval(() => {
+      githubCommand();
+    }, 120000)
   },
+  github: githubCommand,
   discord: async () => {
     const whitelist = discordWL.map((user) => user.userID)
     const idSubmissions = await discord.getIdSubmissions()
